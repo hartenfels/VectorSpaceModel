b339f7b03af969ba1e4a94f0c566eac7a95192ef
Fritz Pope <PapstDonB@Googlemail.com>
2015-06-02 15:39:43 +0200

    Changes formatting of Java code

--- src/main/java/InvertedIndex.java
+++ src/main/java/InvertedIndex.java
@@ -1,50 +1,38 @@
 import java.io.*;
+
 import static java.lang.String.format;
 
 
-public class InvertedIndex implements Serializable
-{
+public class InvertedIndex implements Serializable {
 
-    public void
-    addToken(int id, String token)
-    {
+    public void addToken(int id, String token) {
         throw new RuntimeException("Not implemented.");
     }
 
 
-    public void
-    stash(String path)
-    {
-        try
-        {
-            FileOutputStream   file = new   FileOutputStream(path);
+    public void stash(String path) {
+        try {
+            FileOutputStream file = new FileOutputStream(path);
             ObjectOutputStream objs = new ObjectOutputStream(file);
             objs.writeObject(this);
             objs.close();
             file.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println(format("Error stashing `%s`: %s", path, e.getMessage()));
         }
     }
 
 
-    public static InvertedIndex
-    unstash(String path)
-    {
+    public static InvertedIndex unstash(String path) {
         InvertedIndex index = null;
 
-        try
-        {
-            FileInputStream   file = new FileInputStream(path);
+        try {
+            FileInputStream file = new FileInputStream(path);
             ObjectInputStream objs = new ObjectInputStream(file);
             index = (InvertedIndex) objs.readObject();
             objs.close();
             file.close();
-        }
-        catch (IOException | ClassNotFoundException e)
-        {
+        } catch (IOException | ClassNotFoundException e) {
             System.err.println(format("Error unstashing `%s`: %s", path, e.getMessage()));
         }
 
